477779364bdd4456178d073b95ed214a260bd2c2
Alex <alexmattorr@gmail.com>
2015-02-11 13:52:09 -0800

    working on stuff.

--- proto2/js/base.js
+++ proto2/js/base.js
@@ -1,10 +1,9 @@
 function baseJS() {
 	$(document).ready(function() {
 		function changeTab() {
-			$(".lang").click(function() {
-				$(".is-active").removeClass("is-active");
+			$(".lang").click(function(lang, langItem) {
+				$(".lang").removeClass("is-active");
 				$(this).addClass("is-active");
-				$("lang-items").addClass("is-active");
 			});
 		};
 		changeTab();
