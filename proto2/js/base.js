4c162997e1ef9cc89b1afbc13c32fb23b68687c5
Alex <alexmattorr@gmail.com>
2015-02-18 08:43:15 -0800

    Working tabs.

--- proto2/js/base.js
+++ proto2/js/base.js
@@ -1,9 +1,9 @@
 function baseJS() {
 	$(document).ready(function() {
 		function changeTab() {
-			$(".lang").click(function(lang, langItem) {
-				$(".lang").removeClass("is-active");
-				$(this).addClass("is-active");
+			$(".lang").click(function() {
+				$(".lang").toggleClass("is-active");
+				$(".lang-items").toggleClass("is-active");
 			});
 		};
 		changeTab();
