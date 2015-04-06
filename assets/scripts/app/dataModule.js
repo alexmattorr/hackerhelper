f4493238077511151f64ce626f46c62ac160ca78
Alex <alexmattorr@gmail.com>
2015-04-06 12:50:04 -0700

    adding new pages and begining post production development

--- assets/scripts/app/dataModule.js
+++ assets/scripts/app/dataModule.js
@@ -1,6 +1,40 @@
 var hackerApp = angular.module('hackerApp', []);
 
-hackerApp.controller('cssCtrl', function ($scope) {
+
+hackerApp.controller('langCtrl', function ($scope) {
+	$scope.htmlLang = [
+		{
+			'name': ':active',
+			'url': '../test.html'
+		}, {
+			'name': '::after (:after)',
+			'url': '../test.html'
+		}, {
+			'name': 'align-content',
+			'url': '../test.html'
+		}, {
+			'name': 'align-self',
+			'url': '../test.html'
+		}, {
+			'name': 'all',
+			'url': '../test.html'
+		}, {
+			'name': '<angle>',
+			'url': '../test.html'
+		}, {
+			'name': 'animation',
+			'url': '../test.html'
+		}, {
+			'name': 'animation-delay',
+			'url': '../test.html'
+		}, {
+			'name': 'animation-direction',
+			'url': '../test.html'
+		}, {
+			'name': 'animation-duration',
+			'url': '../test.html'
+		}
+	];
 	$scope.cssLang = [
 		{
 			'name': ':active',
@@ -58,6 +92,72 @@ hackerApp.controller('cssCtrl', function ($scope) {
 			'url': '../test.html'
 		}, {
 			'name': 'operators',
+			'url': '../test.html'
+		}
+	];
+	$scope.jsLang = [
+		{
+			'name': ':active',
+			'url': '../test.html'
+		}, {
+			'name': '::after (:after)',
+			'url': '../test.html'
+		}, {
+			'name': 'align-content',
+			'url': '../test.html'
+		}, {
+			'name': 'align-self',
+			'url': '../test.html'
+		}, {
+			'name': 'all',
+			'url': '../test.html'
+		}, {
+			'name': '<angle>',
+			'url': '../test.html'
+		}, {
+			'name': 'animation',
+			'url': '../test.html'
+		}, {
+			'name': 'animation-delay',
+			'url': '../test.html'
+		}, {
+			'name': 'animation-direction',
+			'url': '../test.html'
+		}, {
+			'name': 'animation-duration',
+			'url': '../test.html'
+		}
+	];
+	$scope.jqLang = [
+		{
+			'name': ':active',
+			'url': '../test.html'
+		}, {
+			'name': '::after (:after)',
+			'url': '../test.html'
+		}, {
+			'name': 'align-content',
+			'url': '../test.html'
+		}, {
+			'name': 'align-self',
+			'url': '../test.html'
+		}, {
+			'name': 'all',
+			'url': '../test.html'
+		}, {
+			'name': '<angle>',
+			'url': '../test.html'
+		}, {
+			'name': 'animation',
+			'url': '../test.html'
+		}, {
+			'name': 'animation-delay',
+			'url': '../test.html'
+		}, {
+			'name': 'animation-direction',
+			'url': '../test.html'
+		}, {
+			'name': 'animation-duration',
 			'url': '../test.html'
 		}
 	];
