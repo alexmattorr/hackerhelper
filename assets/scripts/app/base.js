f7cbe67f9de91b0ec59c8b846cc8b8adbe67f096
Alex <alexmattorr@gmail.com>
2015-04-20 16:37:13 -0700

    Complete restructure.

--- assets/scripts/app/base.js
+++ assets/scripts/app/base.js
@@ -1,13 +1,6 @@
 function baseJS() {
 	$(document).ready(function() {
-/*
-		function loadScreen() {
-			setTimeout(function() {
-				$(".loading-screen").fadeOut();
-				$(".home-wrapper").fadeIn();
-			}, 3000);
-		}
-*/		
+
 		function changeTab() {
 			$(".lang").click(function() {
 				$(".lang").toggleClass("is-active");
@@ -25,18 +18,16 @@ function baseJS() {
 	  		$(".sidebar").removeClass("is-active");
 			});
 		}
-		
-/*
+
 		function goBack() {
 			$(".go-back").on("click", function() {
 	  		window.history.back()
 			});
 		}
-*/
-		// loadScreen();
+
 		changeTab();
 		sideBar();
-		// goBack();
+		goBack();
 	});
 };
 
--- assets/scripts/app/dataModule.js
+++ assets/scripts/app/dataModule.js
@@ -2,178 +2,59 @@ var hackerApp = angular.module('hackerApp', []);
 hackerApp.controller('langCtrl', function ($scope) {
 	$scope.htmlLang = [
 		{
-			'name': ':active',
-			'url': '../test.html'
-		}, {
-			'name': '::after (:after)',
-			'url': '../test.html'
-		}, {
-			'name': 'align-content',
-			'url': '../test.html'
-		}, {
-			'name': 'align-self',
-			'url': '../test.html'
-		}, {
-			'name': 'all',
-			'url': '../test.html'
-		}, {
-			'name': '<angle>',
-			'url': '../test.html'
-		}, {
-			'name': 'animation',
-			'url': '../test.html'
-		}, {
-			'name': 'animation-delay',
-			'url': '../test.html'
-		}, {
-			'name': 'animation-direction',
-			'url': '../test.html'
-		}, {
-			'name': 'animation-duration',
-			'url': '../test.html'
+			name: ':active',
+			url: '../language-property/html/html-test.html'
 		}
 	];
 	$scope.cssLang = [
 		{
 			name: ':active',
-			url: '../test.html',
-			desc: "CSS background properties are used to define the background effects of an element.",
-			descSub: "CSS properties used for background effects:",
-			prop: [
-				'background-color', 
-				'background-image', 
-				'background-repeat', 
-				'background-attachment', 
-				'background-position',
-				'background-clip', 
-				'background-origin'
-			],
-			code: [
-
-			]
+			url: '../language-property/css/css3/css-test.html'
 		}, {
-			'name': '::after (:after)',
-			'url': '../test.html'
+			name: '::after (:after)',
+			url: '../language-property/css/css3/css-test.html'
 		}, {
-			'name': 'align-content',
-			'url': '../test.html'
+			name: 'align-content',
+			url: '../language-property/css/css3/css-test.html'
 		}, {
-			'name': 'align-self',
-			'url': '../test.html'
+			name: 'align-self',
+			url: '../language-property/css/css3/css-test.html'
 		}, {
-			'name': 'all',
-			'url': '../test.html'
+			name: 'all',
+			url: '../language-property/css/css3/css-test.html'
 		}, {
-			'name': '<angle>',
-			'url': '../test.html'
+			name: '<angle>',
+			url: '../language-property/css/css3/css-test.html'
 		}, {
-			'name': 'animation',
-			'url': '../test.html'
+			name: 'animation',
+			url: '../language-property/css/css3/css-test.html'
 		}, {
-			'name': 'animation-delay',
-			'url': '../test.html'
+			name: 'animation-delay',
+			url: '../language-property/css/css3/css-test.html'
 		}, {
-			'name': 'animation-direction',
-			'url': '../test.html'
+			name: 'animation-direction',
+			url: '../language-property/css/css3/css-test.html'
 		}, {
-			'name': 'animation-duration',
-			'url': '../test.html'
+			name: 'animation-duration',
+			url: '../language-property/css/css3/css-test.html'
 		}
 	];
 	$scope.sassLang = [
 		{
-			'name': 'z-index',
-			'url': '../test.html'
-		},{
-			'name': 'preprocessing',
-			'url': '../test.html'
-		}, {
-			'name': 'variables',
-			'url': '../test.html'
-		}, {
-			'name': 'nesting',
-			'url': '../test.html'
-		}, {
-			'name': 'partials',
-			'url': '../test.html'
-		}, {
-			'name': 'import',
-			'url': '../test.html'
-		}, {
-			'name': 'mixins',
-			'url': '../test.html'
-		}, {
-			'name': 'inheritence',
-			'url': '../test.html'
-		}, {
-			'name': 'operators',
-			'url': '../test.html'
+			name: 'placeholder',
+			url: '../language-property/css/sass/sass-test.html'
 		}
 	];
 	$scope.jsLang = [
 		{
-			'name': ':active',
-			'url': '../test.html'
-		}, {
-			'name': '::after (:after)',
-			'url': '../test.html'
-		}, {
-			'name': 'align-content',
-			'url': '../test.html'
-		}, {
-			'name': 'align-self',
-			'url': '../test.html'
-		}, {
-			'name': 'all',
-			'url': '../test.html'
-		}, {
-			'name': '<angle>',
-			'url': '../test.html'
-		}, {
-			'name': 'animation',
-			'url': '../test.html'
-		}, {
-			'name': 'animation-delay',
-			'url': '../test.html'
-		}, {
-			'name': 'animation-direction',
-			'url': '../test.html'
-		}, {
-			'name': 'animation-duration',
-			'url': '../test.html'
+			name: 'placeholder',
+			url: '../language-property/js/js/js-test.html'
 		}
 	];
 	$scope.jqLang = [
 		{
-			'name': ':active',
-			'url': '../test.html'
-		}, {
-			'name': '::after (:after)',
-			'url': '../test.html'
-		}, {
-			'name': 'align-content',
-			'url': '../test.html'
-		}, {
-			'name': 'align-self',
-			'url': '../test.html'
-		}, {
-			'name': 'all',
-			'url': '../test.html'
-		}, {
-			'name': '<angle>',
-			'url': '../test.html'
-		}, {
-			'name': 'animation',
-			'url': '../test.html'
-		}, {
-			'name': 'animation-delay',
-			'url': '../test.html'
-		}, {
-			'name': 'animation-direction',
-			'url': '../test.html'
-		}, {
-			'name': 'animation-duration',
-			'url': '../test.html'
+			name: 'placeholder',
+			url: '../language-property/js/jquery/jquery-test.html'
 		}
 	];
 });
