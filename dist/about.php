3f3d615f60671358ae9a954fe88723c5c344f0df
Alex <alexmattorr@gmail.com>
2015-06-01 13:30:34 -0700

    Revert "create about page"
    This reverts commit 80129ada68e09dae4e72872504cdc00c5b0f32fd.

--- dist/about.php
+++ dist/about.php
@@ -14,25 +14,8 @@
 			<?php include 'components/sidebar.php';?>
 			<section class="content">
 				<article>
-					<div class="main sub-page about">
-							<h1>Hi, We are Hacker Helper</h1>
-							<p>Bacon ipsum dolor amet ball tip porchetta brisket fatback sausage tenderloin spare ribs kevin boudin. Alcatra meatball landjaeger jerky shank hamburger. Beef bresaola ball tip ham chuck tail shoulder. Short loin turkey beef ham hock kielbasa. Beef turkey drumstick rump chuck. Ball tip ribeye brisket, short ribs doner pastrami ham hock ham cow flank strip steak beef ribs porchetta pork chop chicken.</p>
-							<div class="columns">
-								<div class="column">
-									<img src="assets/img/alex.png"/>
-									<h5>Alexander Orr</h5>
-								</div>
-								<div class="column">
-									<img src="assets/img/alex.png"/>
-									<h5>Minnie Tantiphipop</h5>
-								</div>
-								<div class="column">
-									<img src="assets/img/alex.png"/>
-									<h5>Ryan Coughlin</h5>	
-								</div>
-							</div>
-		
-						<h3>We'd love to hear from you!</h3>
+					<div class="main sub-page">
+						<h5>We'd love to hear from you!</h5>
 						<form class="contact-form">
 							<i class="orange fa fa-user"></i><input type="text" placeholder="name">
 							<i class="blue fa fa-envelope-o"></i><input type="text" placeholder="email">
@@ -45,17 +28,6 @@
 							<li><a href="#/"><i class="fa fa-facebook"></i></a></li>
 							<li><a href="#/"><i class="fa fa-twitter"></i></a></li>
 						</ul>
-			
-						<div class="columns">
-							<div class="column">
-								<img src="assets/svg/coffeecup.svg"/>
-							</div>
-							<div class="column">
-								<h4>&lt;LIKE OUR APP?&#47;&gt;</h4>	
-								<h4>&lt;BUY US A CUP OF COFFEE&#47;&gt;</h4>
-								<div class="button paypal"></div>	
-							</div>
-						</div>
 					</div>
 
 				</article>
---
+++ dist/lang/jquery/get.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>hackerhelper</title>
+	<link rel="stylesheet" href="../../assets/css/style.css">
+	<script src="../../assets/js/jquery-2.1.1.min.js"></script>
+	<script src="../../assets/js/app.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<?php include '../../components/lang-header.php';?>
+		<?php include '../../components/lang-sidebar.php';?>
+		<section class="content">
+			<article>
+		<div class="main sub-page">
+			<div class="sub-page-header js">Get & Set</div>
+
+			<h5>Get is used to recieve values from html, set is used to assign</h5>
+			<p class="sub-page-disc">Example of get:</p>
+			<ul class="sub-page-ul js">
+				<li>.text()</li>
+				<li>.html()</li>
+				<li>.val()</li>
+				<li>.attr()</li>
+			</ul>
+
+		<div class="code-block">
+		<pre class="code js">
+<code data-lang="js">
+	<span class="value">
+	$("button").click(function() {
+		console.log( $(h1).text() );
+		console.log( $(h1).text("header") );
+
+		console.log( $(h2).html() );
+		console.log( $(h2).html("subheader") );
+
+		console.log( $('#input').val() );
+		console.log( $('#input').val(6) );
+
+		console.log( $('#home-link').attr("href") );
+		console.log( $('#home-link').attr("index.html") );
+	});
+	</span>
+</code>
+</pre>
+		</div>
+
+		<h5>See Also</h5>
+
+		<ul class="button see-also js">
+<!-- 		
+			<li><a href="functions.php">Functions</a></li>
+			<li><a href="operators.php">Operators</a></li>
+-->
+		</ul>
+		</div>
+
+			</article>
+		</section>
+	</div>
+</body>
+</html>
